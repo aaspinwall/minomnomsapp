e5c69e24b1ddbb04621182644312d66acdbc91df
Alejandro Aspinwall <aaspinwall@gmail.com>
2019-06-17 12:47:01 -0400

    added portions

---
+++ src/Portions.jsx
@@ -0,0 +1,123 @@
+import React from "react";
+import styled from "styled-components";
+import { portions } from "./data";
+
+export default class Portions extends React.Component {
+  constructor() {
+    super();
+    this.state = { verduras: false, cereales: false, grasas: false };
+  }
+  componentDidMount() {}
+  render() {
+    const Space = styled.div`
+      margin: 70px;
+    `;
+    const BoxContainer = styled.div`
+      margin: 1rem;
+      display: flex;
+      flex-wrap: wrap;
+      flex-direction: column;
+      border-radius: 20px;
+      background-color: #2be18325;
+      padding: 1rem;
+
+      @media screen and (min-width: 768px) {
+        display: grid;
+        grid-template-columns: 1fr;
+      }
+    `;
+    const Box = styled.div`
+      display: flex;
+      padding: 0.3rem 0;
+      @media screen and (min-width: 768px) {
+        padding: 0 1rem;
+        display: flex;
+      }
+    `;
+    const BoxHeader = styled(BoxContainer)`
+      text-align: center;
+      font-weight: bolder;
+      margin-bottom: 0;
+    `;
+    const BoxBody = styled(BoxContainer)`
+      display: ${props => (props.show ? "flex" : "none")};
+      @media screen and (min-width: 768px) {
+        display: ${props => (props.show ? "grid" : "none")};
+        grid-template-columns: 1fr 1fr;
+        text-align: justify;
+      }
+    `;
+
+    const verduras = portions.verduras.map(box => {
+      return (
+        <div>
+          <Box>{box}</Box>
+        </div>
+      );
+    });
+    const cereales = portions.cereales.map(box => {
+      return (
+        <div>
+          <Box>{box}</Box>
+        </div>
+      );
+    });
+    const animal = portions.animal.map(box => {
+      return (
+        <div>
+          <Box>{box}</Box>
+        </div>
+      );
+    });
+    const grasas = portions.grasas.map(box => {
+      return (
+        <div>
+          <Box>{box}</Box>
+        </div>
+      );
+    });
+    const bebidas = portions.bebidas.map(box => {
+      return (
+        <div>
+          <Box>{box}</Box>
+        </div>
+      );
+    });
+
+    return (
+      <div>
+        <Space />
+        <BoxHeader
+          onClick={() => this.setState({ verduras: !this.state.verduras })}
+        >
+          🥬 Verduras 🥒
+        </BoxHeader>
+        <BoxBody show={this.state.verduras}>{verduras}</BoxBody>
+        <BoxHeader
+          onClick={() => this.setState({ cereales: !this.state.cereales })}
+        >
+          🍚 Cereales 🍙
+        </BoxHeader>
+        <BoxBody show={this.state.cereales}>{cereales}</BoxBody>
+        <BoxHeader
+          onClick={() => this.setState({ animal: !this.state.animal })}
+        >
+          🍗 Origen Animal 🧀
+        </BoxHeader>
+        <BoxBody show={this.state.animal}>{animal}</BoxBody>
+        <BoxHeader
+          onClick={() => this.setState({ grasas: !this.state.grasas })}
+        >
+          🥑 Grasas 🥜
+        </BoxHeader>
+        <BoxBody show={this.state.grasas}>{grasas}</BoxBody>
+        <BoxHeader
+          onClick={() => this.setState({ bebidas: !this.state.bebidas })}
+        >
+          🥤 Bebidas ☕
+        </BoxHeader>
+        <BoxBody show={this.state.bebidas}>{bebidas}</BoxBody>
+      </div>
+    );
+  }
+}
--- src/index.js
+++ src/index.js
@@ -2,9 +2,23 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import Nav from "./Nav";
+import Portions from "./Portions";
 import * as serviceWorker from "./serviceWorker";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const Test = function() {
+  return <div>Hello</div>;
+};
+
+ReactDOM.render(
+  <Router>
+    <Route path='/' exact={true} component={App} />
+    <Route path='/' exact={false} component={Nav} />
+    <Route path='/portions' exact={true} component={Portions} />
+  </Router>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
