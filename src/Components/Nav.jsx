9f7b25ff2ee4c03b62f430cbcfee34e18accddd1
Alejandro Aspinwall <aaspinwall@gmail.com>
2021-10-15 15:48:33 -0400

    equivalentes

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -1,19 +1,19 @@
-import React from "react";
-import { BrowserRouter, Route, Link } from "react-router-dom";
-import styled from "styled-components";
+import React from "react"
+import { BrowserRouter, Route, Link } from "react-router-dom"
+import styled from "styled-components"
 
-const initialSearch = "Search... Doesn't work for now";
+const initialSearch = "Search... Doesn't work for now"
 
 class Nav extends React.Component {
   constructor(props) {
-    super(props);
-    this.state = { pawState: false, search: initialSearch };
+    super(props)
+    this.state = { pawState: false, search: initialSearch }
   }
   close = () => {
-    this.setState({ pawState: false });
-  };
+    this.setState({ pawState: false })
+  }
   render() {
-    const menuState = this.state.pawState ? "navOpen" : "navClosed ";
+    const menuState = this.state.pawState ? "navOpen" : "navClosed "
     return (
       <nav className='navbar'>
         <img
@@ -33,10 +33,13 @@ class Nav extends React.Component {
           <a className={menuState} href='/menus2.pdf'>
             PDF
           </a>
+          <a onClick={this.close} className={menuState} href='/equivalentes.pdf'>
+            Equivalentes
+          </a>
         </div>
       </nav>
-    );
+    )
   }
 }
 
-export default Nav;
+export default Nav
